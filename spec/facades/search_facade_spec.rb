c28615810b99e311f8ea24c994a60ffb8f7b90e9
Anna Wiley <awiley33@gmail.com>
2024-01-14 00:35:20 -0600

    refactor: Reorganize context blocks in spec file

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -1,39 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe SearchFacade do
-  context "instance methods" do
-    context "#fruit_by_name(name)" do
-      it "can search for fruit by name" do
-        fruit = SearchFacade.new.fruit_by_name("banana")
+  it "can search for fruit by name" do
+    fruit = SearchFacade.new.fruit_by_name("banana")
 
-        expect(fruit).to be_a Fruit
-        expect(fruit.family).to be_a String
-        expect(fruit.order).to be_a String
-        expect(fruit.genus).to be_a String
-        expect(fruit.calories).to be_an Integer
-        expect(fruit.fat).to be_a Float
-        expect(fruit.sugar).to be_a Float
-        expect(fruit.carbohydrates).to be_a Float
-        expect(fruit.protein).to be_a Float
-      end
-    end
+    expect(fruit).to be_a Fruit
+    expect(fruit.family).to be_a String
+    expect(fruit.order).to be_a String
+    expect(fruit.genus).to be_a String
+    expect(fruit.calories).to be_an Integer
+    expect(fruit.fat).to be_a Float
+    expect(fruit.sugar).to be_a Float
+    expect(fruit.carbohydrates).to be_a Float
+    expect(fruit.protein).to be_a Float
+  end
 
-    context "#fruits_by_nutrition(calories)" do
-      it "can search for fruits by calorie count" do
-        fruits = SearchFacade.new.fruits_by_nutrition(70)
-        expect(fruits.count).to eq(10)
+  it "can search for fruits by calorie count" do
+    fruits = SearchFacade.new.fruits_by_nutrition(70)
+    expect(fruits.count).to eq(10)
 
-        fruit = fruits[0]
-        expect(fruit).to be_a Fruit
-        expect(fruit.family).to be_a String
-        expect(fruit.order).to be_a String
-        expect(fruit.genus).to be_a String
-        expect(fruit.calories).to be_an Integer
-        expect(fruit.fat).to be_a Float
-        expect(fruit.sugar).to be_a Float
-        expect(fruit.carbohydrates).to be_a Float
-        expect(fruit.protein).to be_a Float
-      end
-    end
+    fruit = fruits[0]
+    expect(fruit).to be_a Fruit
+    expect(fruit.family).to be_a String
+    expect(fruit.order).to be_a String
+    expect(fruit.genus).to be_a String
+    expect(fruit.calories).to be_an Integer
+    expect(fruit.fat).to be_a Float
+    expect(fruit.sugar).to be_a Float
+    expect(fruit.carbohydrates).to be_a Float
+    expect(fruit.protein).to be_a Float
   end
 end
